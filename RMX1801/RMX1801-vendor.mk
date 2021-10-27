95a270f31276d91e3415e723b20e771ce9b4fb48
daniml3 <danimoral1001@gmail.com>
2021-10-27 17:07:06 +0000

    RMX1801: drop QTI datastatusnotification
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- RMX1801/RMX1801-vendor.mk
+++ RMX1801/RMX1801-vendor.mk
@@ -3194,7 +3194,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
